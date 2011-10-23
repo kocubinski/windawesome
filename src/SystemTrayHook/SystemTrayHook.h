78cc67fca0da57936bba3f28ff93c00b96a9be4d
Alien <none@none>
2011-10-23 17:08:11 +0300

    Merge with multi-monitor-support and remove some problems with conversion from SVN.

--- src/SystemTrayHook/SystemTrayHook.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#define SYSTEMTRAYHOOK_API __declspec(dllexport)
-
-SYSTEMTRAYHOOK_API BOOL __cdecl RegisterSystemTrayHook(HWND hWnd);
-SYSTEMTRAYHOOK_API BOOL __cdecl UnregisterSystemTrayHook();
-
-#undef SYSTEMTRAYHOOK_API
--- src/WindowSubclassing/WindowSubclassing.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#define WINDOWSUBCLASSING_API __declspec(dllexport)
-
-WINDOWSUBCLASSING_API BOOL __cdecl SubclassWindow(HWND hWnd, HWND window);
-WINDOWSUBCLASSING_API BOOL __cdecl UnsubclassWindow(HWND window);
-
-#undef WINDOWSUBCLASSING_API
